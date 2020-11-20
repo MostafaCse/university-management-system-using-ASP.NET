cd40f72b532fb394d9b6ad994ae9c584cdf76a62
Mohammad Golam Mostafa <67516519+MostafaCse@users.noreply.github.com>
2020-11-20 22:23:39 +0600

    Add files via upload

---
+++ UniversityManagementApps/DAL/Models/ViewModels/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NavigateURLWebApps.DAL.Models.ViewModels
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string RegNo { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string code { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
