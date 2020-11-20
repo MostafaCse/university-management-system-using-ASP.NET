c079f756846493a56ce2fb438154a1e2b7607be1
Mohammad Golam Mostafa <67516519+MostafaCse@users.noreply.github.com>
2020-11-20 22:24:48 +0600

    Add files via upload

---
+++ UniversityManagementApps/DAL/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NavigateURLWebApps.DAL.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string code{ get; set; }
+    }
+}
