4eb8348e60a48b0229faefd3f9f97549a3b65f6d
Mohammad Golam Mostafa <67516519+MostafaCse@users.noreply.github.com>
2020-11-20 22:28:56 +0600

    Add files via upload

---
+++ UniversityManagementApps/BLL/DepartmentManager.cs
@@ -0,0 +1,24 @@
+﻿using NavigateURLWebApps.DAL.Gatway;
+using NavigateURLWebApps.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NavigateURLWebApps.BLL
+{
+    public class DepartmentManager
+    {
+        DepartmentGatway DepartmentGatway;
+        public DepartmentManager()
+        {
+            DepartmentGatway = new DepartmentGatway();
+        }
+
+        public List<Department>  GetAllStudent()
+        {
+            return DepartmentGatway.GetAllDepartment();
+        }
+
+    }
+}
---
+++ UniversityManagementApps/BLL/StudentManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NavigateURLWebApps.DAL.Models;
+using NavigateURLWebApps.DAL.Gatway;
+using NavigateURLWebApps.DAL.Models.ViewModels;
+
+namespace NavigateURLWebApps.BLL
+{
+    public class StudentManager
+    {
+        StudentGatway StudentGatway1;
+        public StudentManager()
+        {
+            StudentGatway1 = new StudentGatway();
+        }
+        
+        public string Save(Student student1)
+        {
+
+            if(StudentGatway1.IsRegExists(student1.RegNo)==false)
+          {
+                int rowaffect = StudentGatway1.Save(student1);
+                if (rowaffect > 0)
+                {
+                    return "Save sucessful";
+                }
+                else
+                {
+                    return "Save failed";
+                }
+                
+            }
+
+          else
+          {
+               return "Reg no already exits.";
+          }
+
+        }
+
+        public List<StudentViewModel> GetAllStudents()
+        {
+            List<StudentViewModel > students = StudentGatway1.GetAllStudent();
+            return students;
+        }
+
+        public Student GetStudentById(int id)
+        {
+           return  StudentGatway1.GetStudentById(id);
+        }
+     public string UpdateByStudentId(Student student)
+        {
+            int rowaffect = StudentGatway1.UpdateByStudentId(student);
+
+         if(rowaffect>0)
+         {
+             return "Update successful";
+         }
+         else
+         {
+             return "Update failed";
+         }
+
+        }
+        public string Delete(int id)
+     {
+         int rowaffect = StudentGatway1.Delete(id);
+
+            if(rowaffect>0)
+            {
+                return "Delete sucessful";
+            }
+            else
+            {
+                return "Delete failed"; 
+            }
+     }
+    }
+}
